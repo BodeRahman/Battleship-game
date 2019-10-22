488acb94456a68e2708fced303374ef78f9cd96c
Olagunju Abdulrahman <abdulrahmanolagunju.1@gmail.com>
2019-10-22 13:03:28 +0100

    add the view object to implement the display message of the game

---
+++ battleship.js
@@ -0,0 +1,10 @@
+var view = {
+    displayMessage: function(msg) {
+        var messageArea = document.getElementsById("messageArea");
+        messageArea.innerHTML = msg;
+    },
+    displayHit: function(location) {
+    },
+    displayMiss: function(location) {
+    }
+};
