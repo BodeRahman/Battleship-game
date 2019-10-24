3e62ac8425df81f65b89faf682a4536348b0d32f
Olagunju Abdulrahman <abdulrahmanolagunju.1@gmail.com>
2019-10-24 23:38:55 +0100

    implement the model object

--- battleship.js
+++ battleship.js
@@ -1,14 +1,26 @@
 var view = {
     displayMessage: function(msg) {
-        var messageArea = document.getElementsById("messageArea");
+        var messageArea = document.getElementById("messageArea");
         messageArea.innerHTML = msg;
     },
+
     displayHit: function(location) {
         var cell = document.getElementById(location);
-        call.setAttribute("class","hit")
+        cell.setAttribute("class","hit")
     },
     displayMiss: function(location) {
         var cell = document.getElementById(location);
         cell.setAttribute("class","miss")
     }
+};
+
+var model = {
+    boardSize: 7,
+    numShips: 3,
+    shipLength: 3,
+    shipsSunk: 0,
+
+    ships: [{ locations: ["06", "16", "26"], hits: ["", "", ""] },
+            { locations: ["24", "34", "44"], hits: ["", "", ""] },
+            { locations: ["10", "11", "12"], hits: ["", "", ""] }]
 };
