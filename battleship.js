818593ad723f624f42686296012469066f302a58
Olagunju Abdulrahman <abdulrahmanolagunju.1@gmail.com>
2019-10-25 10:05:24 +0100

    add chaining to the fire method Eliminated the verbosity by grabbing the location array and then combine it to the indexOf method

--- battleship.js
+++ battleship.js
@@ -23,10 +23,16 @@ var model = {
     ships: [{ locations: ["06", "16", "26"], hits: ["", "", ""] },
             { locations: ["24", "34", "44"], hits: ["", "", ""] },
             { locations: ["10", "11", "12"], hits: ["", "", ""] }],
-            
+
     fire: function(guess) {
         for (var i = 0; i < this.numShips; i++) {
             var ship = this.ships[i];
+            var index = ship.locations.indexOf(guess);
+            if (index >= 0) {
+                ship.hits[index] = "hit";
+                return true;
+            }
         }
+        return false;
     }
 };
