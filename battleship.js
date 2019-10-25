ab2c8b07be04480f0dcb8749b178a5be04901d43
Olagunju Abdulrahman <abdulrahmanolagunju.1@gmail.com>
2019-10-25 12:34:33 +0100

    complete model object

--- battleship.js
+++ battleship.js
@@ -3,7 +3,6 @@ var view = {
         var messageArea = document.getElementById("messageArea");
         messageArea.innerHTML = msg;
     },
-
     displayHit: function(location) {
         var cell = document.getElementById(location);
         cell.setAttribute("class","hit")
@@ -13,7 +12,6 @@ var view = {
         cell.setAttribute("class","miss")
     }
 };
-
 var model = {
     boardSize: 7,
     numShips: 3,
@@ -23,16 +21,31 @@ var model = {
     ships: [{ locations: ["06", "16", "26"], hits: ["", "", ""] },
             { locations: ["24", "34", "44"], hits: ["", "", ""] },
             { locations: ["10", "11", "12"], hits: ["", "", ""] }],
-
     fire: function(guess) {
         for (var i = 0; i < this.numShips; i++) {
             var ship = this.ships[i];
             var index = ship.locations.indexOf(guess);
             if (index >= 0) {
                 ship.hits[index] = "hit";
+                view.displayHit(guess);
+                view.displayMessage("HIT!");
+                if (this.shipsSunk(ship)) {
+                    view.displayMessage("You sank my battleship!");
+                    this.shipsSunk++;
+                }
                 return true;
             }
         }
+        view.displayMiss(guess);
+        view.displayMessage("You missed.");
         return false;
+    },
+    isSunk: function(ship) {
+        for (var i = 0;i < this.shipLength; i++) {
+            if (ship.hits[i] !== "hit"){
+                return false;
+            }
+        }
+        return true;
     }
 };
