1fc207b680a07c30b8fb2243b63e832219ba2dd0
Olagunju Abdulrahman <abdulrahmanolagunju.1@gmail.com>
2019-11-03 11:55:27 +0100

    remove hardcoded ship locations and add the call to generate the ship locations

--- battleship.js
+++ battleship.js
@@ -19,9 +19,9 @@ var model = {
     shipLength: 3,
     shipsSunk: 0,
 
-    ships: [{ locations: ["06", "16", "26"], hits: ["", "", ""] },
-            { locations: ["24", "34", "44"], hits: ["", "", ""] },
-            { locations: ["10", "11", "12"], hits: ["", "", ""] }],
+    ships: [{ locations: ["0", "0", "0"], hits: ["", "", ""] },
+            { locations: ["0", "0", "0"], hits: ["", "", ""] },
+            { locations: ["0", "0", "0"], hits: ["", "", ""] }],
 
     fire: function(guess) {
         for (var i = 0; i < this.numShips; i++) {
@@ -142,6 +142,8 @@ function init() {
     fireButton.onclick = handleFireButton;
     var guessInput = document.getElementById("guessInput");
     guessInput.onkeypress = handleKeyPress;
+
+    model.generateShipLocations();
 }
 
 function handleFireButton() {
